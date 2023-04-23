17d558e811a4cb2b0bf0ef414a6c8f24fb172e1e
DanBd22 <dan.badarau04@gmail.com>
2023-04-23 13:44:42 +0300

    Initial Commit

---
+++ MyFifthProject/MyFifthProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFifthProjectApp.swift
+//  MyFifthProject
+//
+//  Created by Badarau Dan on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFifthProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
